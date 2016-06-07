629aa902a6017f4f06f1761a50e4d25d74457828
Longyang Zhang <zhanglongyang.cn@gmail.com>
2016-06-07 18:46:23 +0800

    separate routers in server.js to another file

---
+++ server-side-development/node-examples/src/express-example/server-separate-router.js
@@ -0,0 +1,17 @@
+var express = require('express')
+var morgan = require('morgan')
+var dishRouter = require('./routes/dishRouter')
+
+var hostname = 'localhost'
+var port = 3000
+
+var app = express()
+app.use(morgan('dev'))
+
+app.use('/dishes', dishRouter)
+
+app.use(express.static(__dirname + '/public'))
+
+app.listen(port, hostname, function(){
+  console.log(`Server running at http://${hostname}:${port}/`)
+})
