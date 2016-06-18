ec06e6681b9fd02f068600719cf80b2a4f726ac4
Longyang Zhang <zhanglongyang.cn@gmail.com>
2016-06-19 07:36:46 +1000

    finish basic-auth

--- server-side-development/node-examples/src/basic-auth/server.js
+++ server-side-development/node-examples/src/basic-auth/server.js
@@ -16,9 +16,32 @@ function auth(req, res, next) {
         next(err)
         return
     }
+
+    var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':')
+    var user = auth[0]
+    var pass = auth[1]
+
+    if (user === 'admin' && pass === 'password') {
+        next()
+    } else {
+        var err = new Error('You are not authenticated')
+        err.status = 401
+        next(err)
+    }
 }
 
+app.use(auth)
+
 app.use(express.static(__dirname + '/public'))
+
+app.use(function(err, req, res, next) {
+    res.writeHead(err.status || 500, {
+        'WWW-Authenticate': 'Basic',
+        'Content-Type': 'text/plain'
+    })
+    res.end(err.message)
+})
+
 app.listen(port, hostname, function() {
     console.log(`server running at http://${hostname}:${port}`)
 })
