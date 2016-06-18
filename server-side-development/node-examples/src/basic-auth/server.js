c21bfbacb47ca1eaf09e1d2f6d716c6647b2ce4b
Longyang Zhang <zhanglongyang.cn@gmail.com>
2016-06-19 09:19:47 +1000

    add cookie-parser to basic-auth

--- server-side-development/node-examples/src/basic-auth/server.js
+++ server-side-development/node-examples/src/basic-auth/server.js
@@ -1,4 +1,5 @@
 var express = require('express')
+var cookieParser = require('cookie-parser')
 var morgan = require('morgan')
 
 var hostname = 'localhost'
@@ -6,28 +7,42 @@ var port = 3000
 
 var app = express()
 app.use(morgan('dev'))
+app.use(cookieParser('12345-67890-09876-54321'))
 
 function auth(req, res, next) {
-    var authHeader = req.headers.authorization
+    if (!req.signedCookies.user) {
+        var authHeader = req.headers.authorization
 
-    if (!authHeader) {
-        var err = new Error('You are not authenticated')
-        err.status = 401
-        next(err)
-        return
-    }
+        if (!authHeader) {
+            var err = new Error('You are not authenticated')
+            err.status = 401
+            next(err)
+            return
+        }
 
-    var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':')
-    var user = auth[0]
-    var pass = auth[1]
+        var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':')
+        var user = auth[0]
+        var pass = auth[1]
 
-    if (user === 'admin' && pass === 'password') {
-        next()
+        if (user === 'admin' && pass === 'password') {
+            res.cookie('user', 'admin', {signed: true})
+            next()
+        } else {
+            var err = new Error('You are not authenticated')
+            err.status = 401
+            next(err)
+        }
     } else {
-        var err = new Error('You are not authenticated')
-        err.status = 401
-        next(err)
+        if (req.signedCookies.user === 'admin') {
+            console.log(req.signedCookies)
+            next()
+        } else {
+            var err = new Error('You are not authenticated')
+            err.status = 401
+            next(err)
+        }
     }
+
 }
 
 app.use(auth)
