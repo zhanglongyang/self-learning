1f68aa84ac745c2a6eb2faeb93ceb90dc04ca568
Longyang Zhang <zhanglongyang.cn@gmail.com>
2016-06-29 20:35:26 +1000

    start learning akka

---
+++ learning-akka/hello-akka/src/main/scala/HelloAkka.scala
@@ -0,0 +1,19 @@
+import akka.actor.{Actor, ActorSystem, Props}
+
+// Actor message
+case class WhoToGreet(who: String)
+
+// Actor
+class Greeter extends Actor {
+  override def receive: Receive = {
+    case WhoToGreet(who) => println(s"Hello $who")
+  }
+}
+
+object HelloAkka extends App {
+  val system = ActorSystem("Hello-Akka")
+
+  val greeter = system.actorOf(Props[Greeter], "greeter")
+
+  greeter ! WhoToGreet("Akka")
+}
