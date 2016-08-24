f7f53bb4300d2568cde64c50a45cd7dc5c69ec6f
Longyang Zhang <zhanglongyang.cn@gmail.com>
2016-08-24 20:55:32 +1000

    use neo to init todoApp

---
+++ getting-started-with-redux/todoApp/src/routes.js
@@ -0,0 +1,9 @@
+import Home from './home';
+import About from './about';
+import Todo from './todo'
+
+export default [
+  { path: '/', title: 'Home', component: Home },
+  { path: '/about', title: 'About', component: About },
+  { path: '/todo', title: 'Todo', component: Todo }
+];
---
+++ getting-started-with-redux/todoApp/src/todo/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Todo() {
+  return (
+    <div>
+      Todos
+    </div>
+  )
+}
+
+Todo.displayName = "Todo"
