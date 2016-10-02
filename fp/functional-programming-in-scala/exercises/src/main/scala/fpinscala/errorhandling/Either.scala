f61785977e02bd23dc99b7af92bf66dd0509ea0e
Longyang Zhang <zhanglongyang.cn@gmail.com>
2016-10-02 15:09:21 +1100

    finish Either

--- fp/functional-programming-in-scala/exercises/src/main/scala/fpinscala/errorhandling/Either.scala
+++ fp/functional-programming-in-scala/exercises/src/main/scala/fpinscala/errorhandling/Either.scala
@@ -4,21 +4,54 @@ package fpinscala.errorhandling
 import scala.{Option => _, Either => _, Left => _, Right => _, _} // hide std library `Option` and `Either`, since we are writing our own in this chapter
 
 sealed trait Either[+E,+A] {
- def map[B](f: A => B): Either[E, B] = sys.error("todo")
+ def map[B](f: A => B): Either[E, B] = {
+   this match {
+     case Right(a) => Right(f(a))
+     case _ => _
+   }
+ }
 
- def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = sys.error("todo")
+ def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = {
+   this match {
+     case Right(a) => f(a)
+     case _ => _
+   }
+ }
 
- def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = sys.error("todo")
+ def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = {
+   this match {
+     case Left(_) => b
+     case _ => _
+   }
+ }
 
- def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = sys.error("todo")
+ def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = {
+   this.flatMap(a => b.map(f(a, _)))
+ }
 }
 case class Left[+E](get: E) extends Either[E,Nothing]
 case class Right[+A](get: A) extends Either[Nothing,A]
 
 object Either {
-  def traverse[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] = sys.error("todo")
+  def traverse[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] = {
+    es match {
+      case Nil => Right(Nil)
+      case h :: t => for {
+        hh <- f(h)
+        tt <- traverse(t)(f)
+      } yield (hh :: tt)
+    }
+  }
 
-  def sequence[E,A](es: List[Either[E,A]]): Either[E,List[A]] = sys.error("todo")
+  def sequence[E,A](es: List[Either[E,A]]): Either[E,List[A]] = {
+    es match {
+      case Nil => Right(Nil)
+      case h :: t => for {
+        hh <- h
+        tt <- sequence(t)
+      } yield (hh :: tt)
+    }
+  }
 
   def mean(xs: IndexedSeq[Double]): Either[String, Double] = 
     if (xs.isEmpty) 
