4cbc14b1d9930c1d35042d7bd28d3a9d2ee06142
Longyang Zhang <zhanglongyang.cn@gmail.com>
2017-05-26 21:04:04 +1000

    add tensorflow hello-world

---
+++ ai/tensorflow/installation_test.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+
+sess = tf.Session()
+
+hello = tf.constant("Hello from TensorFlow")
+
+print(sess.run(hello))
+
+a = tf.constant(20)
+b = tf.constant(22)
+print('a + b = {0}'.format(sess.run(a + b)))
