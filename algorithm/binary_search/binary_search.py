8d24d6fc2fb4a6df24870c746c6eb2cd4c7aefe7
Longyang Zhang <zhanglongyang.cn@gmail.com>
2017-12-28 16:34:16 +1100

    add binary search python implementation

---
+++ algorithm/binary_search/binary_search.py
@@ -0,0 +1,22 @@
+def binary_search(l, item):
+    low = 0
+    high = len(l) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        guess = l[mid]
+
+        if guess == item:
+            return mid
+
+        if guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+
+    return None
+
+print(binary_search([1, 3, 5, 7, 9], 3))
+print(binary_search([1, 3, 5, 7, 9], 5))
+print(binary_search([1, 3, 5, 7, 9], 6))
